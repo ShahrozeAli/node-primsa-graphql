602e1c306b1e22446b7c3b0205e85f77cf7c9d70
Shahroze Ali <shehrozegoo1@gmail.com>
2024-02-12 09:34:28 +0500

    testing prisma over postgres

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+// app.ts
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { ApolloServer, gql } from "apollo-server";
+import fs from "fs";
+
+// Read the GraphQL schema file
+const typeDefs = fs.readFileSync("./graphql/schema.graphql", "utf-8");
+
+// Import resolvers
+const resolvers = require("./graphql/resolvers");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create Express application
+const app = express();
+
+// Set up middleware to parse JSON requests
+app.use(express.json());
+
+// Define a simple route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript!");
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// Create an Apollo server
+const server = new ApolloServer({
+  typeDefs: gql(typeDefs),
+  resolvers,
+});
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
